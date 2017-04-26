edc5523a3cc219046543bd29edb9cfc596596ec3
bjweiqm <weimegn@126.com>
2017-04-26 11:30:39 +0800

    demo

---
+++ day01/list_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+"""
+@software: PyCharm
+@file: list_demo.py
+@time: 2017/4/26 11:10
+
+list 内置方法学习练习
+"""
+
---
+++ day01/task_demo_01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+"""
+@software: PyCharm
+@file: task_demo_01.py
+@time: 2017/4/26 11:11
+1. 用户登录
+2. 输入密码错误3次锁死
+"""
