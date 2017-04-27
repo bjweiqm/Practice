4345c62e38f1ffd392a67a4f68964b887366c66e
bjweiqm <weimegn@126.com>
2017-04-27 10:48:22 +0800

    猜数字

---
+++ day01/guess_number.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+"""
+@software: PyCharm
+@file: guess_number.py
+@time: 2017/4/27 10:17
+猜数字游戏
+"""
+lucky_num = 19
+input_num = -1
+guess_count = 0
+
+while guess_count < 3:
+    input_num = int(raw_input('input the guess num:'))
+    if input_num > lucky_num:
+        print '大了...'
+    elif input_num < lucky_num:
+        print '小了...'
+    else:
+        print '恭喜...'
+        break
+    guess_count += 1
+else:
+    print 'lower'
